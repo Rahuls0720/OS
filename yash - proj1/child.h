0605a54f742bf70d7d297d0cd3b4ad391f55d075
rahuls0720 <rahuls0720@gmail.com>
2018-07-08 15:30:51 -0700

    move yash to its own repo

--- yash - proj1/child.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <string.h>
-#include <signal.h>
-#include <sys/types.h>
-
-int nonpipeFunc(char *arr[], int *fileRedir, int fg);
--- yash - proj1/childchild.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <string.h>
-#include <signal.h>
-#include <sys/types.h>
-
-int pipeFunc(char *arr[], int Pindex, int *fileRedir, int fg);
